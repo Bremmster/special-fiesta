0d9a5e435b6455cb1dc7a330d9b3a7dca5d2dae8
Kristian Karlson <83krikar@gafe.molndal.se>
2023-11-15 15:56:22 +0100

    bookController ready for testing

--- src/main/java/com/karlson/crudapi/controller/BookController.java
+++ src/main/java/com/karlson/crudapi/controller/BookController.java
@@ -2,11 +2,12 @@ package com.karlson.crudapi.controller;
 
 import com.karlson.crudapi.model.Book;
 import com.karlson.crudapi.repository.BookRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -19,12 +20,36 @@ public class BookController {
     }
 
     //    C - Insert
+    @PostMapping("") // todo ad validation
+    public void create(@RequestBody Book book) {
+        repository.save(book);
+    }
 
-//    R - SelectOne
+    //    R - SelectOne
+    @GetMapping("/{id}") // todo return a response body
+    public Optional<Book> findById(@PathVariable Integer id) {
+        return repository.findById(id);
+    }
 
-//    U - Update
 
-//    D - Delete
+    //    U - Update
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Book book, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found!");
+        }
+        repository.save(book);
+    }
+
+
+    //    D - Delete
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
+
 
     //    L - SelectAll
     @RequestMapping("")
