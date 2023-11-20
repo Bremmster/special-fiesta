354998bab4248f9dd2c7f48d271a6a95a4388127
Kristian Karlson <83krikar@gafe.molndal.se>
2023-11-20 18:02:29 +0100

    added profiles and spring doc documentation (#1)
    * works but everything is unlocked
    
    * boot profiles no boot with different security chains
    
    * improved profiles and closed down warnings

--- src/main/java/com/karlson/crudapi/config/SecurityConfig.java
+++ src/main/java/com/karlson/crudapi/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.karlson.crudapi.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,8 @@ public class SecurityConfig {
 
     private static final String API_URL_PATTERN = "api/v1/**";
     private static final String HOME_PATTERN = "/";
+    private static final String SWAGER_UI_PATTERN = "/swagger-ui/**";
+    private static final String API_DOCS = "/api-docs/**";
 /*
     @Bean
     @Deprecated
@@ -30,8 +33,9 @@ public class SecurityConfig {
 
  */
 
-
+    /* dev profile uses a H2 database.       When the console is enabled the issues with "multiple servlets" This is a workaround for that */
     @Bean
+    @Profile("dev")
     // https://stackoverflow.com/questions/77024398/spring-h2db-web-console-this-method-cannot-decide-whether-these-patterns-are
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
@@ -41,19 +45,24 @@ public class SecurityConfig {
                                 .ignoringRequestMatchers(
                                         mvcMatcherBuilder.pattern(API_URL_PATTERN), // todo make this correct, implementing correct metods.
                                         mvcMatcherBuilder.pattern(HOME_PATTERN),
+                                        mvcMatcherBuilder.pattern(SWAGER_UI_PATTERN),
+                                        mvcMatcherBuilder.pattern(API_DOCS),
                                         PathRequest.toH2Console()))
 
                 .headers(headersConfigurer ->
                         headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
 
                 .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN)).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, HOME_PATTERN)).permitAll()
-                                //This line is optional in .authenticated() case as .anyRequest().authenticated()
-                                //would be applied for H2 path anyway
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .anyRequest().authenticated()
+                                auth
+                                        .requestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN)).permitAll()
+                                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, SWAGER_UI_PATTERN)).permitAll()
+                                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, API_DOCS)).permitAll()
+                                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.GET, HOME_PATTERN)).permitAll()
+                                        //This line is optional in .authenticated() case as .anyRequest().authenticated()
+                                        //would be applied for H2 path anyway
+                                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+//                                .anyRequest().authenticated() // this locks things down
+                                        .anyRequest().permitAll() // todo remove
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(Customizer.withDefaults())
@@ -62,8 +71,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /*
+
     @Bean
+    @Profile("prod")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> {
@@ -78,6 +88,4 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
-*/
-
 }
