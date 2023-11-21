4747e4729d7ff15a6e2c93aab93ec22bf5468c84
Kristian Karlson <83krikar@gafe.molndal.se>
2023-11-21 13:45:33 +0100

    removed unused import

--- src/test/java/com/karlson/crudapi/HomeControllerTest.java
+++ src/test/java/com/karlson/crudapi/HomeControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
