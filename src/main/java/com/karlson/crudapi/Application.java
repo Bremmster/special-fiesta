8e4765de4ace6fffa620e80c73b97da4040c5435
Kristian Karlson <83krikar@gafe.molndal.se>
2023-11-21 10:39:22 +0100

    implementing jwt tokens

--- src/main/java/com/karlson/crudapi/Application.java
+++ src/main/java/com/karlson/crudapi/Application.java
@@ -1,11 +1,12 @@
 package com.karlson.crudapi;
 
 import com.karlson.crudapi.config.CrudAPIProperties;
+import com.karlson.crudapi.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(CrudAPIProperties.class)
+@EnableConfigurationProperties({CrudAPIProperties.class, RsaKeyProperties.class})
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/karlson/crudapi/config/SecurityConfig.java
+++ src/main/java/com/karlson/crudapi/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.karlson.crudapi.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +15,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -18,22 +33,28 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     private static final String API_URL_PATTERN = "api/v1/**";
     private static final String HOME_PATTERN = "/";
     private static final String SWAGER_UI_PATTERN = "/swagger-ui/**";
     private static final String API_DOCS = "/api-docs/**";
-/*
-    @Bean
-    @Deprecated
-    MvcRequestMatcher.Builder h2mvc(HandlerMappingIntrospector introspector) {
-        return new MvcRequestMatcher.Builder(introspector)
-                .servletPath("/h2-console/*");
+    private final RsaKeyProperties rsaKey;
+
+    @Autowired
+    public SecurityConfig(RsaKeyProperties rsaKey) {
+        this.rsaKey = rsaKey;
     }
 
- */
+    @Bean
+    public InMemoryUserDetailsManager user() { // todo come back here and look
+        return new InMemoryUserDetailsManager(
+                User.withUsername("usr")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
 
-    /* dev profile uses a H2 database.       When the console is enabled the issues with "multiple servlets" This is a workaround for that */
+    /* dev profile uses a H2 database. When the console is enabled the issues with "multiple servlets" This is a workaround for that */
     @Bean
     @Profile("dev")
     // https://stackoverflow.com/questions/77024398/spring-h2db-web-console-this-method-cannot-decide-whether-these-patterns-are
@@ -79,13 +100,27 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/").permitAll();
                     auth.requestMatchers(HttpMethod.POST, "/api/v1/books/").permitAll();
-                    auth.requestMatchers(HttpMethod.GET, "/api/v1/books/").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "/api/v1/books/*").permitAll();
                     auth.requestMatchers(HttpMethod.PUT, "/api/v1/books/").authenticated();
+                    auth.requestMatchers(HttpMethod.DELETE, "/api/v1/books/*").authenticated();
                     auth.anyRequest().authenticated();
                 })
-//                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKey.publicKey()).privateKey(rsaKey.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
